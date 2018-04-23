48bee45d99f2024bea01767dab2475f5df12c8d6
lanceris <lanceris.777@gmail.com>
2018-04-23 19:40:14 +0300

    Release

--- excel.py
+++ excel.py
@@ -1,8 +1,8 @@
 import asyncio
+import json
 import logging
 import sys
 
-import aiohttp
 from aiohttp import ClientSession
 from openpyxl import load_workbook
 from sqlalchemy import Column, Integer, Float, String, DateTime, create_engine
@@ -12,12 +12,24 @@ from sqlalchemy.orm import sessionmaker
 import utils
 
 config = utils.config
-def_log = utils.def_log
-con_log = utils.con_log
-
-MAX_CONNECTIONS = 100
+loggers = [utils.def_log, utils.con_log]
+Base = declarative_base()
 
 
+class Monitoring(Base):
+
+    __tablename__ = 'monitoring'
+
+    id = Column('id', Integer, primary_key=True)
+    ts = Column('timestamp', DateTime)
+    url = Column('url', String, nullable=False)
+    label = Column('label', String, nullable=False)
+    response_time = Column('response_time', Float)
+    status_code = Column('status_code', Integer)
+    content_length = Column('content_length', Integer)
+
+
+@utils.log_time(loggers)
 def process_xlsx(filename):
     wb = load_workbook(filename=filename)
     ws = wb.active
@@ -30,36 +42,109 @@ def process_xlsx(filename):
         if ws[f'{config["fetch_col"]}{n}'].value == 1:
             urls_to_fetch.append((ws[f'{config["url_col"]}{n}'].value, ws[f'{config["label_col"]}{n}'].value))
         n += 1
+
+    msg = f"Processed {sys.argv[1]}, found {len(urls_to_fetch)} urls to fetch."
+    [logger.log(logging.INFO, msg) for logger in loggers]
+
     return urls_to_fetch
 
 
-async def process_urls(session):
-    main_queue = asyncio.Queue(maxsize=1000)
+async def process_urls(session, urls, amount):
+    main_queue = asyncio.Queue(maxsize=500)
     responses = []
-    urls_to_fetch = process_xlsx(sys.argv[1])[:5]
+    errors = []
+
+    if len(urls) >= amount:
+        urls_to_fetch = urls[:amount]
+    else:
+        urls_to_fetch = urls
+
     # we init the consumers, as the queues are empty at first,
     # they will be blocked on the main_queue.get()
-    consumers = [asyncio.ensure_future(consumer(main_queue=main_queue,
-                                                session=session,
-                                                responses=responses)) for _ in range(min(MAX_CONNECTION,
-                                                                                         len(urls_to_fetch)))]
-    await producer(queue=main_queue, urls_to_fetch=urls_to_fetch)
-    # wait for all item's inside the main_queue to get task_done
+    consumers = [asyncio.ensure_future(utils.consumer(main_queue=main_queue,
+                                                      session=session,
+                                                      responses=responses,
+                                                      errors=errors))
+                 for _ in range(amount)]
+
+    await utils.producer(queue=main_queue, urls_to_fetch=urls_to_fetch)
+    # wait for all items inside the main_queue to get task_done
     await main_queue.join()
     # cancel all coroutines
     for consumer_future in consumers:
         consumer_future.cancel()
-    print(responses)
+
+    msg = f"Processed {len(urls_to_fetch)} urls, {len(errors)} errors total."
+    [logger.log(logging.INFO, msg) for logger in loggers]
+
+    process_errors(errors)
     return responses
 
 
+@utils.log_time(loggers)
+def process_errors(errors):
+    try:
+
+        with open(config['error_log_path'], 'r+') as j:
+            try:
+                msg = f"Trying to load data from error log({config['error_log_path']})"
+                [logger.log(logging.DEBUG, msg) for logger in loggers]
+
+                data = json.load(j)
+                j.seek(0)
+                j.truncate()
+                data.get('entries').append(errors)
+                json.dump(data, j, indent=4)
+
+                msg = f"Successfully logged {len(errors)} errors."
+                [logger.log(logging.INFO, msg) for logger in loggers]
+
+            except json.JSONDecodeError:
+                msg = f"Can't decode error log({config['error_log_path']}). Creating empty one..."
+                [logger.log(logging.WARNING, msg) for logger in loggers]
+                json.dump({'entries': errors}, j, indent=4)
+    except FileNotFoundError:
+        msg = "No error log found, creating new..."
+        [logger.log(logging.DEBUG, msg) for logger in loggers]
+        with open(config['error_log_path'], 'w'):
+            pass
+        process_errors(errors)
+
+
+@utils.log_time(loggers)
+def save_to_db(data_list):
+    engine = create_engine(f'sqlite:///{config["db_path"]}')
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    db_session = Session()
+    for each in data_list:
+        monitoring = Monitoring(**each)
+        db_session.add(monitoring)
+    db_session.commit()
+    db_session.close()
+
+    msg = f"Saved {len(data_list)} entries to {config['db_path']}."
+    [logger.log(logging.INFO, msg) for logger in loggers]
+
+
 async def run(loop):
-    # we init more connectors to get better performance
-    async with ClientSession(loop=loop, connector=aiohttp.TCPConnector(limit=MAX_CONNECTIONS)) as session:
-        resps = await process_urls(session)
-    return resps
+    try:
+        amount = sys.argv[2]
+    except IndexError:
+        amount = config['urls_amount']
+
+    urls_to_fetch = process_xlsx(sys.argv[1])
+
+    async with ClientSession(loop=loop) as session:
+        resps = await process_urls(session, urls=urls_to_fetch, amount=amount)
+
+    save_to_db(resps)
 
 
-if __name__ == '__main__':
+@utils.log_time(loggers)
+def main():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run(loop))
+
+if __name__ == '__main__':
+    main()
--- utils.py
+++ utils.py
@@ -2,8 +2,7 @@ from datetime import datetime
 import json
 import logging
 from time import time
-
-import async_timeout
+import traceback
 
 
 def setup_logger(name,
@@ -29,10 +28,12 @@ def log_time(loggers):
             ts = time()
             result = method(*args, **kw)
             te = time()
-            for logger in loggers:
-                logger.log(logging.INFO, '{}: {:.2f} ms'.format(method.__name__, round((te-ts)*1000, 2)))
+            try: # check if loggers is iterable
+                for logger in loggers:
+                    logger.log(logging.INFO, '{}: {:.2f} ms'.format(method.__name__, round((te - ts) * 1000, 2)))
+            except TypeError: # not iterable
+                loggers.log(logging.INFO, '{}: {:.2f} ms'.format(method.__name__, round((te - ts) * 1000, 2)))
             return result
-
         return timed
     return timeit
 
@@ -50,18 +51,18 @@ def load_config(config_path='config.json'):
                         "request_timeout": 1,
                         "error_log_path": "error_dump.json",
                         "log_path": "log.log",
-                        "db_path": "sqlite3.db"
+                        "db_path": "sqlite3.db",
+                        "urls_amount": 100
                      }
             json.dump(config, json_config, indent=4)
     return config
 
 
-async def consumer(main_queue, session, responses):
+async def consumer(main_queue, session, responses, errors):
     start = time()
+    url = await main_queue.get()
     try:
-        url = await main_queue.get()
-        with async_timeout.timeout(5):
-            async with session.get(url[0]) as response:
+        async with session.head(url[0], timeout=1) as response:
                 dct = dict()
                 dct['ts'] = datetime.now()
                 dct['url'] = str(response.url)
@@ -72,27 +73,38 @@ async def consumer(main_queue, session, responses):
                     if response.status == 200 else None
                 responses.append(dct)
                 main_queue.task_done()
-    # In case of a time in our get request/ problem with response code
     except Exception as e:
-        print(e)  # HANDLE EXCEPTION
+        error = {
+            'timestamp': str(datetime.now().strftime('%d/%m/%Y %H:%M:%S')),
+            'url': str(url),
+            'error':
+                {
+                    'exception_type': str(e.__class__.__name__),
+                    'exception_value': str(e),
+                    'stack_info': str(traceback.format_exc())
+                }
+        }
+        errors.append(error)
         main_queue.task_done()
 
 
 async def producer(queue, urls_to_fetch):
     for url in urls_to_fetch:
-        # if the queue is full(reached maxsize) this line will be blocked
+        # if the queue is full, this line will be blocked
         # until a consumer will finish processing a url
         await queue.put(url)
+
+
 
 
 config = load_config()
 con_log = setup_logger('con_log',
                        log_handler=logging.StreamHandler,
-                       log_format="%(name)-12s: %(levelname)-8s %(message)s")
+                       log_date_format='[%H:%M:%S]')
 def_log = setup_logger('def_log',
                        log_handler=logging.FileHandler,
                        log_file=config['log_path'],
-                       log_date_format='[%H:%M:%S]')
+                       log_date_format='%d/%m/%Y [%H:%M:%S]')
 
 
 
